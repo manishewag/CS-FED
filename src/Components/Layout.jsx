23b75b4c7aae76c41a9863f94147e4d24f3f8543
manishewag <160532465+manishewag@users.noreply.github.com>
2024-11-05 11:19:23 +0530

    last commit

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
+import IndexPage from './IndexPage'
+
 
 function Layout() {
   return (
     <div className='p-4 '>
         <Header/>
+        <IndexPage/>
         <Outlet/>
     </div>
   )
