b0fd406d732f7f78a364996df5f23ab93b4fdbaf
manishewag <160532465+manishewag@users.noreply.github.com>
2024-11-18 22:54:24 +0530

    last commit

--- src/Components/BookingPage.jsx
+++ src/Components/BookingPage.jsx
@@ -9,13 +9,14 @@ import BookingDates from "./BookingDates";
 
 export default function BookingPage() {
     const {id} = useParams();
-    const [booking,setBooking] = useState(null);
+    // const [booking,setBooking] = useState(null);
+    const [booking, setBooking] = useState({});
    
   useEffect(() => {
     if (id) {
       axios.get('/bookings').then(response => {
         // const foundBooking = response.data.find(({_id}) => _id === id);
-        const foundBooking = response.data.find((booking) => booking._id === id)
+        const foundBooking = response.data.fillter((booking) => booking._id === id)
         if (foundBooking) {
           setBooking(foundBooking);
         //   {headers: {
